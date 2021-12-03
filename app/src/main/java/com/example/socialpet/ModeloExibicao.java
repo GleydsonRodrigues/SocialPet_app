0a43e74d8e09e4f2522d2c5f868c6d609384015a
Gleydson <rgleydson794@gmail.com>
2021-12-03 19:45:23 -0300

    Primeira etapa do projeto finalizada

--- app/src/main/java/com/example/socialpet/ModeloExibicao.java
+++ app/src/main/java/com/example/socialpet/ModeloExibicao.java
@@ -1,5 +1,6 @@
 package com.example.socialpet;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,8 +16,9 @@ public class ModeloExibicao extends AppCompatActivity {
 
     private String nome, imgPost, imgPerfil, preco, descricao;
     private ImageView imgPostExibicao, imgPerfilExibicao;
-    private TextView txtNome, txtDescricao, txtPreco;
+    private TextView txtNome, txtDescricao, txtPreco, lblPreco;
     private Button btnVoltarExibicao;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +30,8 @@ public class ModeloExibicao extends AppCompatActivity {
         txtNome = findViewById(R.id.txtNomeUsuarioPostExibicao);
         txtDescricao = findViewById(R.id.txtDescricaoExibicao);
         btnVoltarExibicao = findViewById(R.id.btnVoltarExibicao);
+        txtPreco = findViewById(R.id.txtPrecoExibicao);
+        lblPreco = findViewById(R.id.lblPrecoExibicao);
 //        txtPreco = findViewById(R.id.txtP
 
         Bundle extra = getIntent().getExtras();
@@ -46,6 +50,15 @@ public class ModeloExibicao extends AppCompatActivity {
             Picasso.get()
                     .load(imgPost)
                     .into(imgPostExibicao);
+
+            if (preco != null){
+
+                txtPreco.setVisibility(View.VISIBLE);
+                lblPreco.setVisibility(View.VISIBLE);
+
+                txtPreco.setText("R$" + preco +",00");
+
+            }
         }
 
         btnVoltarExibicao.setOnClickListener(new View.OnClickListener() {
